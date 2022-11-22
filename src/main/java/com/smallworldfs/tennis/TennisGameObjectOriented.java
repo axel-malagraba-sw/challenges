231bffdc3373949939773e904cc9b374a574191d
Axel Malagraba <axel.malagraba@smallworldfs.com>
2022-11-22 13:38:32 +0100

    refactor constructor

--- src/main/java/com/smallworldfs/tennis/TennisGameObjectOriented.java
+++ src/main/java/com/smallworldfs/tennis/TennisGameObjectOriented.java
@@ -9,20 +9,13 @@ import java.util.List;
 
 public class TennisGameObjectOriented implements TennisGame {
 
-    private final Player player1;
-    private final Player player2;
-    private final List<GameState> specialStates;
-    private final GameState baseState;
-
-    public TennisGameObjectOriented() {
-        player1 = new Player("player1");
-        player2 = new Player("player2");
-        baseState = new GameState(player1, player2);
-        specialStates = List.of(
-                new WinningState(player1, player2),
-                new AdvantageState(player1, player2),
-                new TieState(player1, player2));
-    }
+    private final Player player1 = new Player("player1");
+    private final Player player2 = new Player("player2");
+    private final GameState baseState = new GameState(player1, player2);
+    private final List<GameState> specialStates = List.of(
+            new WinningState(player1, player2),
+            new AdvantageState(player1, player2),
+            new TieState(player1, player2));
 
     public String getScore() {
         return specialStates.stream()
